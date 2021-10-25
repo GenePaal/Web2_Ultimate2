0676d9634245c79ed3cc20d00a7d777dd798a410
GenePaal <1968315997@qq.com>
2021-10-25 20:34:52 +0800

    tortoise svn 测试

---
+++ ch01/app/src/main/java/pers/jp/ch01/MainActivity.java
@@ -0,0 +1,15 @@
+package pers.jp.ch01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int a = android.R.color.secondary_text_light;
+    }
+}
